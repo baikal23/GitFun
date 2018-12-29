ac5686887c385c18c37d6c43f97cf017543e0ddb
Sue Kohler <skohler00@gmail.com>
2018-12-28 21:00:23 -0500

    new message for branch one

--- GitFun/ViewController.swift
+++ GitFun/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Original code")
         print("BranchOne")
+        print("New message for branch one")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
